5e6e5c4a9017bdc4a204283ae4f67b55b02ae001
Salma <salmaabdelmotagaly390@gmail.com>
2025-11-29 16:38:16 +0200

    chore(middleware): configure request body limits and add mongoSanitize

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import helmet from 'helmet';
-import fs from 'fs';
 import morgan from 'morgan';
+import cors from 'cors';
 import path from 'path';
+import fs from 'fs';
+import mongoSanitize from 'express-mongo-sanitize';
 import errorHandler from './common/errors/errorHandler';
 import limiter from './config/limiter';
 
@@ -13,9 +14,12 @@ const logStream = fs.createWriteStream(path.join(process.cwd(), 'logs', 'api.log
   flags: 'a',
 });
 
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny', { stream: logStream }));
+app.use(mongoSanitize());
 app.use(limiter);
 
 app.get('/', async (req, res) => {
